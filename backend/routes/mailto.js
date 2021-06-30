e17a07b36d96ea1563c4047e89564dcd8016fed2
akolosov <a.kolosov@digital-lab.ru>
2021-06-30 16:26:28 +0500

    f

--- backend/routes/mailto.js
+++ backend/routes/mailto.js
@@ -4,23 +4,23 @@ const nodemailer = require('nodemailer');
 const router = require('express').Router();
 
 const transport = nodemailer.createTransport(smtpTransport({
-    service: 'gmail',
     auth: {
-        user: process.env.MAIL_USER || 'andreywayne97@gmail.com',
-        pass: process.env.MAIL_PASSWORD || 'ipadib1488'
+        user: '74autoturbo@gmail.com',
+        pass:'ipadib339'
+    },
+    host: 'smtp.gmail.com',
+    port: 465,
+    tls: {
+        rejectUnauthorized:true
     },
     secure: false,
-    requireTLS: true, //Force TLS
-    tls: {
-        rejectUnauthorized: false
-    },
 }));
 
 router.post('/mailto', async (req, res) => {
     const body = req.body;
     try {
         const result = await transport.sendMail({
-            from: `"Сообщение с сайта TURBO" <'andreywayne97@gmail.com'>`,
+            from: `74autoturbo@gmail.com`,
             to: '74autoturbo@gmail.com',
             html: `
                 <strong>Имя: ${body.name}</strong>
@@ -33,8 +33,6 @@ router.post('/mailto', async (req, res) => {
                 <p>${body.message}</p>
             `,
         });
-
-        console.log(result)
 
         res.json('ok');
     } catch (error) {
