d4c8a3fed3f70a2393b10d0f72e94b9250cc55f0
AndreyWayne <wayne.andrey2016@yandex.ru>
2025-10-31 00:03:17 +0500

    fix

--- dump.sql
+++ dump.sql
@@ -5,15 +5,14 @@
 -- Dumped from database version 10.5 (Debian 10.5-1.pgdg90+1)
 -- Dumped by pg_dump version 10.5 (Debian 10.5-1.pgdg90+1)
 
+-- PostgreSQL database dump
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
+-- SET transaction_timeout = '10s';  -- Закомментировано, чтобы избежать ошибки
+SET set_config = 'search_path = public, pg_catalog';
 
 --
 -- Name: DATABASE postgres; Type: COMMENT; Schema: -; Owner: postgres
